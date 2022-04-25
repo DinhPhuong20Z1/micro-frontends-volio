2b8325463050b7e05236a0abbbe132bc07b6401f
kimpv <kimpv@volio.vn>
2022-04-25 17:46:30 +0700

    Footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss'],
-  template: `
-    <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
-    </span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
-  `,
+    selector: 'ngx-footer',
+    styleUrls: ['./footer.component.scss'],
+    template: `
+        <span class="created-by">
+            Created with ♥ by <b><a href="https://volio.vn" target="_blank">Volio</a></b> 2022
+        </span>
+        <div class="socials">
+            <a href="https://volio.vn" target="_blank" class="ion ion-social"><img src="assets/images/volio.png"></a>
+            <a href="https://www.facebook.com/volio.vn" target="_blank" class="ion ion-social-facebook"></a>
+            <a href="https://www.linkedin.com/company/voliovietnam" target="_blank" class="ion ion-social-linkedin"></a>
+        </div>`,
 })
 export class FooterComponent {
 }
